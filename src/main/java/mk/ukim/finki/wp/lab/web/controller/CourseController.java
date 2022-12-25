fafb1db6eac6d9a89e897891826ee6dc6ea9b5f9
MartinAdzhiev <adzhievmartin@gmail.com>
2022-12-25 20:57:10 +0100

    Fix edit course and sorting courses by name

--- src/main/java/mk/ukim/finki/wp/lab/web/controller/CourseController.java
+++ src/main/java/mk/ukim/finki/wp/lab/web/controller/CourseController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-Fimport java.util.Comparator;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
